13f3024a221294800ce0250ed152ca5cb4ad013a
Muhammad Baheuddeen <muhammad.baheuddeen@gmail.com>
2024-10-23 18:59:59 +0300

    add product

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -4,14 +4,29 @@ import path from 'path';
 
 const absPath = path.resolve();
 const dataPath = path.resolve(absPath, 'products.json');
-const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8').toString());
 const router = Router();
 
 router.get('/products', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8').toString());
+
   return res.json(data);
 })
 
+router.get('/products/:id', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8').toString());
+
+  const { id } = req.params;
+  const product = data.find((product: any) => product.id == id);
+  if (!product) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
+  return res.json(product);
+})
+
 router.post('/products/:id', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8').toString());
+
   const { id } = req.params;
   const product = data.find((product: any) => product.id == id);
   const body = req.body;
@@ -26,7 +41,7 @@ router.post('/products/:id', async (req, res) => {
       }
       return product;
     });
-  }
+  }  
 
   fs.writeFileSync(dataPath, JSON.stringify(updatedData, null, 2));
 
