4e8e50e09004e0e30b006f4ef524f1bcc6e8555b
Muhammad Baheuddeen <muhammad.baheuddeen@gmail.com>
2024-10-23 18:50:02 +0300

    update data

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -9,6 +9,28 @@ const router = Router();
 
 router.get('/products', async (req, res) => {
   return res.json(data);
+})
+
+router.post('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = data.find((product: any) => product.id == id);
+  const body = req.body;
+  let updatedData = [];
+
+  if (!product) {
+    updatedData = [...data, body];
+  } else {
+    updatedData = data.map((product: any) => {
+      if (product.id == id) {      
+        return body;
+      }
+      return product;
+    });
+  }
+
+  fs.writeFileSync(dataPath, JSON.stringify(updatedData, null, 2));
+
+  return res.json(body);
 });
 
 export default router
